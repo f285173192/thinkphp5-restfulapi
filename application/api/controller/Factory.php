1d3e84f96ca102a4037316c2858b4e3db3290495
comment <285173192@qq.com>
2018-07-31 10:54:51 +0800

    增加22行注释
    增加22行注释

--- application/api/controller/Factory.php
+++ application/api/controller/Factory.php
@@ -20,6 +20,7 @@ class Factory
         if (!isset(self::$Factory[$className]) || !self::$Factory[$className]) {
             self::$Factory[$className] = new $className($options);
          //self::$Factory[$className]只是一个存放对象的容器,也可以 self::$Factory
+         //em:$a; $a['aa']=1111; echo $a['aa'];//输出1111
         }
         return self::$Factory[$className];
     }
